bbb601798a047294a7c2f47bd05cdf12404a6896
Dan Matthews <dan@bluefoc.us>
2014-11-25 09:21:59 -0500

    Adding auto-shutdown handlers [Resolves Issue #15]

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -5,23 +5,24 @@ describe Emque::Consuming::Application do
     it "does not trigger a shutdown if the limit has not been reached" do
       Dummy::Application.config.error_limit = 2
       app = Dummy::Application.new
-      Emque::Consuming::Runner.instance = double(:router)
+      Emque::Consuming::Runner.instance = double(:runner)
 
       expect(Emque::Consuming::Runner.instance).to_not receive(:stop)
 
-      app.notice_error({ :test => 'failure' })
+      app.notice_error({ :test => "failure" })
     end
 
     it "triggers a shutdown one the error_limit is reached" do
       Dummy::Application.config.error_limit = 2
       app = Dummy::Application.new
-      Emque::Consuming::Runner.instance = double(:router)
+      Emque::Consuming::Runner.instance =
+        double(:runner, :status => double(:status, :to_h => {}))
 
       expect(Emque::Consuming::Runner.instance)
         .to receive(:stop).exactly(1).times
 
-      app.notice_error({ :test => 'failure' })
-      app.notice_error({ :test => 'another failure' })
+      app.notice_error({ :test => "failure" })
+      app.notice_error({ :test => "another failure" })
     end
   end
 end
