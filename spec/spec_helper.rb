1d33f92312bb9c981afea8454910be81b3c4c0bf
Shane Emmons <shane@emmons.io>
2015-02-17 02:46:29 +0000

    silence some of the spec noise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,10 +28,16 @@ Timecop.safe_mode = true
 
 RSpec.configure do |config|
   config.order = "random"
-
   config.include VerifyAndResetHelpers
 
   config.after(:each) do
     FileUtils.remove_dir("dummy/tmp", true)
+  end
+
+  config.around do |example|
+    original_stdout = $stdout
+    $stdout = StringIO.new
+    example.run
+    $stdout = original_stdout
   end
 end
