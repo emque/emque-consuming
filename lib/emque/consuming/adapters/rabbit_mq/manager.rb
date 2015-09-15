316399927dd71810236d382626266d474877bd4b
Ryan Williams <ryan.williams@teamsnap.com>
2015-09-15 00:35:05 +0000

    Utilize single error exchange/queue per service
    All of a service's errors will go into a single queue. These messages
    that triggered an error can then inspected via the RabbitMQ admin UI,
    reviewed, and then removed or manually replublished as needed.

--- lib/emque/consuming/adapters/rabbit_mq/manager.rb
+++ lib/emque/consuming/adapters/rabbit_mq/manager.rb
@@ -17,6 +17,7 @@ module Emque
 
           def start
             setup_connection
+            initialize_error_queue
             initialize_workers
             logger.info "RabbitMQ Manager: starting #{worker_count} workers..."
             workers(:flatten => true).each do |worker|
@@ -67,6 +68,20 @@ module Emque
                 end
               end
             }
+          end
+
+          def initialize_error_queue
+            channel = @connection.create_channel
+            error_exchange = channel.fanout(
+              "#{config.app_name}.error",
+              :durable => true,
+              :auto_delete => false
+            )
+            channel.queue(
+              "#{config.app_name}.error",
+              :durable => true,
+              :auto_delete => false,
+            ).bind(error_exchange)
           end
 
           def new_worker(topic)
