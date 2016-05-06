cb3735f620ce047d89dafcba95b44e750128bd74
Tyler Knappe <tyler.knappe@teamsnap.com>
2016-05-06 21:14:52 +0000

    Allow the number of delayed message workers to be configured.

--- lib/emque/consuming/adapters/rabbit_mq/manager.rb
+++ lib/emque/consuming/adapters/rabbit_mq/manager.rb
@@ -10,7 +10,7 @@ module Emque
 
           def actor_died(actor, reason)
             unless shutdown
-              logger.error "RabbitMQ Manager: actor_died - #{actor.inspect} "+
+              logger.error "RabbitMQ Manager: actor_died - #{actor.inspect} " +
                            "died: #{reason}"
             end
           end
@@ -19,10 +19,15 @@ module Emque
             setup_connection
             initialize_error_queue
             initialize_workers
-            initialize_delayed_message_workers
+            initialize_delayed_message_workers if enable_delayed_message
             logger.info "RabbitMQ Manager: starting #{worker_count} workers..."
             workers(:flatten => true).each do |worker|
               worker.async.start
+            end
+            if enable_delayed_message
+              delayed_message_workers.each do |worker|
+                worker.async.start
+              end
             end
           end
 
@@ -33,6 +38,12 @@ module Emque
               workers(:flatten => true).each do |worker|
                 logger.info "RabbitMQ Manager: stopping #{worker.topic} worker..."
                 worker.stop
+              end
+              if enable_delayed_message
+                delayed_message_workers.each_with_index do |worker, i|
+                  logger.info "RabbitMQ Manager: stopping #{worker.class} #{i + 1} worker..."
+                  worker.stop
+                end
               end
             end
 
@@ -56,13 +67,17 @@ module Emque
             flatten ? @workers.values.flatten : @workers
           end
 
+          def delayed_message_workers
+            @delayed_message_workers
+          end
+
           def retry_errors
             RetryWorker.new(@connection).retry_errors
           end
 
           private
 
-          attr_writer :workers
+          attr_writer :workers, :delayed_message_workers
 
           def initialize_workers
             self.workers = {}.tap { |workers|
@@ -76,7 +91,11 @@ module Emque
           end
 
           def initialize_delayed_message_workers
-            DelayedMessageWorker.new_link(@connection).async.start
+            self.delayed_message_workers = [].tap { |workers|
+              config.delayed_message_workers.times do
+                workers << DelayedMessageWorker.new_link(@connection)
+              end
+            }
           end
 
           def enable_delayed_message
--- lib/emque/consuming/configuration.rb
+++ lib/emque/consuming/configuration.rb
@@ -3,27 +3,29 @@ require "logger"
 module Emque
   module Consuming
     class Configuration
-      attr_accessor :app_name, :adapter, :auto_shutdown, :enable_delayed_message
-        :error_handlers, :error_limit, :error_expiration, :retryable_errors,
+      attr_accessor :app_name, :adapter, :auto_shutdown,
+        :delayed_message_workers, :enable_delayed_message, :error_handlers,
+        :error_limit, :error_expiration, :retryable_errors,
         :retryable_error_limit, :status, :status_port, :status_host,
         :socket_path, :shutdown_handlers
       attr_writer :env, :log_level
 
       def initialize
-        @app_name               = ""
-        @auto_shutdown      = false
-        @enable_delayed_message = false
-        @error_handlers         = []
-        @error_limit            = 5
-        @error_expiration       = 3600 # 60 minutes
-        @log_level              = nil
-        @retryable_errors       = []
-        @retryable_error_limit  = 3
-        @status_port            = 10000
-        @status_host            = "0.0.0.0"
-        @status                 = :off # :on
-        @socket_path            = "tmp/emque.sock"
-        @shutdown_handlers      = []
+        @app_name                = ""
+        @auto_shutdown           = false
+        @delayed_message_workers = 1
+        @enable_delayed_message  = false
+        @error_handlers          = []
+        @error_limit             = 5
+        @error_expiration        = 3600 # 60 minutes
+        @log_level               = nil
+        @retryable_errors        = []
+        @retryable_error_limit   = 3
+        @status_port             = 10000
+        @status_host             = "0.0.0.0"
+        @status                  = :off # :on
+        @socket_path             = "tmp/emque.sock"
+        @shutdown_handlers       = []
       end
 
       def env
@@ -48,6 +50,7 @@ module Emque
             :app_name,
             :auto_shutdown,
             :adapter,
+            :delayed_message_workers,
             :env,
             :enable_delayed_message,
             :error_handlers,
